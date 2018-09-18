1a3f06fffffac34e818b65ae73a7fd761b513c9c
Jonah Minihan <jonahminihan@gmail.com>
2018-09-18 10:26:20 -0500

    added team info

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -9,8 +9,9 @@ using System;
 public class PlayerHealth : NetworkBehaviour {
 
     public const int maxHealth = 100;
+    //[SyncVar]
+    System.Random rand = new System.Random();
     [SyncVar]
-    //System.Random rand = new System.Random();
     public int currentHealth = maxHealth;
     public bool destroyOnDeath;
     public GameObject[] spawnPoints;
@@ -51,22 +52,23 @@ public class PlayerHealth : NetworkBehaviour {
     {
         if (isLocalPlayer)
         {
-            //int randomInt = rand.Next(0, spawnPoints.Length -1);
+            int randomInt = rand.Next(0, spawnPoints.Length -1);
             // move back to zero location
             transform.position = Vector3.zero;
             // spawnPoints.
-            //Vector3 vect = new Vector3();
-            //vect = spawnPoints[randomInt].GetComponent<Transform>().position;
-            /*vect.x = spawnPoints[randomInt].GetComponent<Transform>().position.x;
+            Vector3 vect = new Vector3();
+            Quaternion quat = new Quaternion();
+            vect = spawnPoints[randomInt].GetComponent<Transform>().position;
+            vect.x = spawnPoints[randomInt].GetComponent<Transform>().position.x;
             vect.y = spawnPoints[randomInt].GetComponent<Transform>().position.y;
             vect.z = spawnPoints[randomInt].GetComponent<Transform>().position.z;
             transform.position = vect;
 
-            vect.x = spawnPoints[randomInt].GetComponent<Transform>().rotation.x;
-            vect.x = spawnPoints[randomInt].GetComponent<Transform>().rotation.y;
-            vect.x = spawnPoints[randomInt].GetComponent<Transform>().rotation.z;
-            transform.rotation = vect;
-            */
+            quat.x = spawnPoints[randomInt].GetComponent<Transform>().rotation.x;
+            //quat.y = spawnPoints[randomInt].GetComponent<Transform>().rotation.y;
+            //quat.z = spawnPoints[randomInt].GetComponent<Transform>().rotation.z;
+            //transform.rotation = quat;
+            /**/
             //rb.MovePosition(Vector3.zero);
         }
     }
---
+++ Assets/Scripts/TeamScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamScript : MonoBehaviour {
+
+
+    public int team = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+    public int getTeam (){
+        if (team == 1){
+            team = 0;
+        }
+        else {
+            team = 1;
+        }
+        return team;
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
