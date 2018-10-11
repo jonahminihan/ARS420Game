f5135488730030b733642980091dae41b64a95db
maw0041 <maw0041@uah.edu>
2018-10-11 10:40:33 -0500

    added alpha map, pro builder, leader and score board

---
+++ Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaderboard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int team0score, team1score;
+	}
+	
+	// Update is called once per frame
+	public void updatescore (int update)
+    {
+		
+	}
+}
